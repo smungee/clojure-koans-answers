5324a2283e7715c1689d8a8f88ff81bb063fbb82
Sumedh Mungee <smungee@gmail.com>
2014-01-11 22:29:35 -0800

    solved up to 12

--- src/koans/12_creating_functions.clj
+++ src/koans/12_creating_functions.clj
@@ -2,7 +2,7 @@
 
 (meditations
   "One may know what they seek by knowing what they do not seek"
-  (= [false true false] (let [not-a-symbol? (complement symbol?)]
+  (= [true false true] (let [not-a-symbol? (complement symbol?)]
                   (map not-a-symbol? [:a 'b "c"])))
 
   "Praise and 'complement' may help you separate the wheat from the chaff"
@@ -12,21 +12,21 @@
 
   "Partial functions allow procrastination"
   (= 20 (let [multiply-by-5 (partial * 5)]
-          (___ __)))
+          (multiply-by-5 4)))
 
   "Don't forget: first things first"
-  (= [__ __ __ __]
+  (= [:a :b :a :b]
        (let [ab-adder (partial concat [:a :b])]
-         (ab-adder [__ __])))
+         (ab-adder [:a :b])))
 
   "Functions can join forces as one 'composed' function"
-  (= 25 (let [inc-and-square (comp square inc)]
-          (inc-and-square __)))
+  (= 36 (let [inc-and-square (comp square inc)]
+          (inc-and-square 5)))
 
   "Have a go on a double dec-er"
-  (= __ (let [double-dec (comp dec dec)]
+  (= 8 (let [double-dec (comp dec dec)]
           (double-dec 10)))
 
   "Be careful about the order in which you mix your functions"
-  (= 99 (let [square-and-dec ___]
+  (= 99 (let [square-and-dec (comp dec square)]
           (square-and-dec 10))))
